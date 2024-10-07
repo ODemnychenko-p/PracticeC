b4e7534a728f58715ca52a8ddf945487629b6674
oleksandr.demnychenko <oleksandr.demnychenko@fracturedbyte.com>
2024-10-07 18:29:17 +0300

    Added new tasks

--- StringsAndRegex/main.cpp
+++ StringsAndRegex/main.cpp
@@ -4,8 +4,11 @@
 #include "task_number_4.h"
 #include "task_number_5.h"
 #include "task_number_6.h"
+#include "task_number_7.h"
+#include "task_number_8.h"
+#include "task_number_9.h"
 
 int main()
 {
-    task_number_6();
+    task_number_9();
 }
---
+++ StringsAndRegex/task_number_9.h
@@ -0,0 +1,19 @@
+//
+// Created by O.Demnychenko on 10/7/2024.
+//
+
+#include <string>
+#include <regex>
+#include <cassert>
+
+std::string transform_date(std::string_view text)
+{
+    auto rx = std::regex{ R"((\d{1,2})(\.|-|/)(\d{1,2})(\.|-|/)(\d{4}))" };
+    return std::regex_replace(text.data(), rx, R"($5-$3-$1)");
+}
+
+static void task_number_9()
+{
+    using namespace std::string_literals;
+    assert(transform_date("today is 01.12.2017!"s) == "today is 2017-12-01!"s);
+}
