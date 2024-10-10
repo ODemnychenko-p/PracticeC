277d8e6c5fcff4845e6ac08e7cde0a15f35bd75f
bolhar <s.demnichenko@gmail.com>
2024-10-10 10:23:54 +0300

    Added new tasks file system

---
+++ FileSystems/task_number_7.h
@@ -0,0 +1,8 @@
+//
+// Created by O.Demnychenko on 10/10/2024.
+//
+
+#ifndef PRACTICEC___TASK_NUMBER_7_H
+#define PRACTICEC___TASK_NUMBER_7_H
+
+#endif //PRACTICEC___TASK_NUMBER_7_H
---
+++ FileSystems/task_number_8.h
@@ -0,0 +1,8 @@
+//
+// Created by O.Demnychenko on 10/10/2024.
+//
+
+#ifndef PRACTICEC___TASK_NUMBER_8_H
+#define PRACTICEC___TASK_NUMBER_8_H
+
+#endif //PRACTICEC___TASK_NUMBER_8_H
